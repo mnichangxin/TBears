4a59886c0ef6a72401074ac15a8577408110bc5b
mnichangxin <mnichangxin@163.com>
2020-02-01 22:37:26 +0800

    Add page

--- styles/notice/addFriends.js
+++ styles/notice/addFriends.js
@@ -3,8 +3,7 @@ import { scaleSize, scaleFont } from '@utils/scaleUtil'
 
 export default StyleSheet.create({
     root: {
-        flexGrow: 1,
-        // backgroundColor: '#ffffff'
+        flexGrow: 1
     },
     addFriendsWrapper: {
         flex: 1,
@@ -14,10 +13,27 @@ export default StyleSheet.create({
         backgroundColor: '#ffffff'
     },
     addFriendsSearchWrapper: {
-        paddingTop: scaleSize(132),
+        paddingTop: scaleSize(90),
         paddingBottom: scaleSize(30),
         paddingLeft: scaleSize(55),
         paddingRight: scaleSize(55)
+    },
+    addFriendsSearchIcon: {
+        position: 'absolute',
+        top: scaleSize(90 + 35),
+        left: scaleSize(55 + 37),
+        width: scaleSize(53),
+        height: scaleSize(53),
+        zIndex: 100
+    },
+    addFriendsSearchInput: {
+        paddingTop: scaleSize(35),
+        paddingBottom: scaleSize(35),
+        paddingLeft: scaleSize(37 + 53 + 18),
+        paddingRight: scaleSize(37),
+        borderRadius: scaleSize(12),
+        fontSize: scaleFont(39),
+        backgroundColor: '#F5F5F5'
     },
     addFriendsNavFeatures: {
         paddingLeft: scaleSize(55),
@@ -31,34 +47,74 @@ export default StyleSheet.create({
     addFriendsNavFeatureText: {
         fontSize: scaleFont(42),
         color: '#333333'
+    },
+    addFriendsListItemRowFrontWrapper: {
+        paddingLeft: scaleSize(55),
+        paddingRight: scaleSize(55),
+        backgroundColor: '#ffffff' 
+    },
+    addFriendsListItemTimeline: {
+        paddingTop: scaleSize(31),
+        paddingBottom: 0
+    },
+    addFriendsListItemTimelineText: {
+        fontSize: scaleFont(42),
+        color: '#333333'
+    },
+    addFriendsListItemRowFront: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: scaleSize(65),
+        paddingBottom: scaleSize(45),
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2'
+    },
+    addFriendsListItemFrontLeft: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    addFriendsListItemAvatar: {
+        width: scaleSize(132),
+        height: scaleSize(132)
+    },
+    addFriendsListItemAvatar: {
+        width: scaleSize(132),
+        height: scaleSize(132)
+    },
+    addFriendsListItemHint: {
+        marginLeft: scaleSize(42)
+    },
+    addFriendsListItemName: {
+        fontSize: scaleFont(42),
+        color: '#333333'
+    },
+    addFriendsListItemOverview: {
+        marginTop: scaleSize(16),
+        fontSize: scaleFont(39),
+        color: '#999999'
+    },
+    addFriendsListItemFrontRight: {
+        alignItems: 'flex-end'
+    },
+    addFriendsListItemTime: {
+        fontSize: scaleFont(39),
+        color: '#999999'
+    },
+    verifyStatusText: {
+        fontSize: scaleFont(39),
+        color: '#999999'
+    },
+    verifyStatusBtn: {
+        paddingTop: scaleSize(7),
+        paddingBottom: scaleSize(7),
+        paddingLeft: scaleSize(30),
+        paddingRight: scaleSize(30),
+        borderRadius: scaleSize(8),
+        backgroundColor: '#E2E1E8'
+    },
+    verifyStatusBtnText: {
+        fontSize: scaleFont(39),
+        color: '#897FDD'
     }
-
-    // friendsList: {
-    //     flex: 1
-    // },
-    // friendsListItemWrapper: {
-    //     paddingLeft: scaleSize(55),
-    //     paddingRight: scaleSize(55)
-    // },
-    // friendsListItem: {
-    //     flexDirection: 'row',
-    //     alignItems: 'center',
-    //     paddingTop: scaleSize(65),
-    //     paddingBottom: scaleSize(45),
-    //     borderBottomWidth: 1,
-    //     borderBottomColor: '#f2f2f2'
-    // },
-    // friendsListItemLeft: {
-    //     flexDirection: 'row',
-    //     alignItems: 'center'
-    // },
-    // friendsListItemAvatar: {
-    //     width: scaleSize(132),
-    //     height: scaleSize(132)
-    // },
-    // friendsListItemNickname: {
-    //     marginLeft: scaleSize(42),
-    //     fontSize: scaleFont(48),
-    //     color: '#333333'
-    // }
 })
--- views/notice/addFriends.js
+++ views/notice/addFriends.js
@@ -3,70 +3,120 @@
  */
 
 import React from 'react'
-import { View, Text, Image, ScrollView, TextInput, FlatList } from 'react-native'
+import { View, Text, Image, TextInput, SectionList, TouchableOpacity } from 'react-native'
+import { scaleSize } from '@utils/scaleUtil'
 import Header from '@views/common/header'
 import styles from '@styles/notice/addFriends'
 
 export default props => {
-    const data = [{
-        key: '1',
-        avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/add-friends.png',
-    }, {
-        key: '2',
-        avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
-        nickname: '李二麻子'
-    }, {
-        key: '3',
-        avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
-        nickname: '李二麻子'
-    }, {
-        key: '4',
-        avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
-        nickname: '李二麻子'
-    }, {
-        key: '5',
-        avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
-        nickname: '李二麻子'
-    }]
-    const renderFriendsListItem = data => (
-        <View style={styles.friendsListItemWrapper}>
-            <View style={styles.friendsListItem}>
-                <View style={styles.friendsListItemLeft}>
-                    <Image
-                        style={styles.friendsListItemAvatar}
-                        source={{ uri: data.item.avatarUri }} />
-                    <Text style={styles.friendsListItemNickname}>
-                        { data.item.key === '1' ? '新的朋友' : data.item.nickname }
-                    </Text>
+    const renderFriendsNav = () => (
+        <View style={styles.addFriendsNav}>
+            <View style={styles.addFriendsSearchWrapper}>
+                <Image
+                    source={{ uri: 'http://q4pas9fmo.bkt.clouddn.com/search.png' }}
+                    style={styles.addFriendsSearchIcon} />
+                <TextInput 
+                    style={styles.addFriendsSearchInput}
+                    placeholder="输入手机号/探熊号"
+                    placeholderTextColor="#999999" />
+            </View>
+            <View style={styles.addFriendsNavFeatures}>
+                <View style={[styles.addFriendsNavFeature, { borderBottomWidth: 1, borderBottomColor: '#f2f2f2' }]}>
+                    <Text style={styles.addFriendsNavFeatureText}>扫一扫</Text>
+                    <View></View>
+                </View>
+                <View style={styles.addFriendsNavFeature}>
+                    <Text style={styles.addFriendsNavFeatureText}>手机联系人</Text>
+                    <View></View>
                 </View>
             </View>
         </View>
     )
+
+    const sections = [{
+        key: '1',
+        data: [{}],
+        renderItem: renderFriendsNav
+    }, {
+        key: '2',
+        timeline: '近三天',
+        data: [{
+            avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
+            nickname: '李二麻子',
+            verifyMessage: '李二麻子你好',
+            verifyStatus: 1
+        }, {
+            avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
+            nickname: '李二麻子',
+            verifyMessage: '李二麻子你好',
+            verifyStatus: 1
+        }]
+    }, {
+        key: '3',
+        timeline: '三天前',
+        data: [{
+            avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
+            nickname: '李二麻子',
+            verifyMessage: '李二麻子你好',
+            verifyStatus: 1
+        }, {
+            avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
+            nickname: '李二麻子',
+            verifyMessage: '李二麻子你好',
+            verifyStatus: 1
+        }, {
+            avatarUri: 'http://q4pas9fmo.bkt.clouddn.com/group-avatar.png',
+            nickname: '李二麻子',
+            verifyMessage: '李二麻子你好',
+            verifyStatus: 0
+        }]
+    }]
+    
+    const renderItem = ({ item, index, section }) => (
+        <View style={[styles.addFriendsListItemRowFrontWrapper, index == 0 && { marginTop: scaleSize(24) }]}>
+            {
+                index == 0 &&
+                <View style={styles.addFriendsListItemTimeline}>
+                    <Text style={styles.addFriendsListItemTimelineText}>{ section.timeline }</Text>
+                </View>
+            }
+            <View style={styles.addFriendsListItemRowFront}>
+                <View style={styles.addFriendsListItemFrontLeft}>
+                    <Image 
+                        source={{ uri: item.avatarUri }}
+                        style={styles.addFriendsListItemAvatar} />
+                    <View style={styles.addFriendsListItemHint}>
+                        <Text style={styles.addFriendsListItemName}>{ item.nickname }</Text>
+                        <Text style={styles.addFriendsListItemOverview}>{ item.verifyMessage }</Text>
+                    </View>
+                </View>
+                <View style={styles.addFriendsListItemFrontRight}>
+                    {
+                        item.verifyStatus == 1
+                        ? 
+                        <Text style={styles.verifyStatusText}>已添加</Text>
+                        : 
+                        <TouchableOpacity activeOpacity={.6}>
+                            <View style={styles.verifyStatusBtn}>
+                                <Text style={styles.verifyStatusBtnText}>查看</Text>
+                            </View>
+                        </TouchableOpacity>
+                    }
+                    
+                </View>  
+            </View>
+        </View>
+    )
+
     return (
         <View style={styles.root}>
             <Header { ...props } title="挖好友" />
-            <ScrollView style={styles.addFriendsWrapper}>
-                <View style={styles.addFriendsNav}>
-                    <View style={styles.addFriendsSearchWrapper}>
-                        <TextInput />
-                    </View>
-                    <View style={styles.addFriendsNavFeatures}>
-                        <View style={[styles.addFriendsNavFeature, { borderBottomWidth: 1, borderBottomColor: '#f2f2f2' }]}>
-                            <Text style={styles.addFriendsNavFeatureText}>扫一扫</Text>
-                            <View></View>
-                        </View>
-                        <View style={styles.addFriendsNavFeature}>
-                            <Text style={styles.addFriendsNavFeatureText}>手机联系人</Text>
-                            <View></View>
-                        </View>
-                    </View>
-                </View>
-                {/* <FlatList 
-                    style={styles.friendsList}
-                    data={data}
-                    showsVerticalScrollIndicator={false}
-                    renderItem={renderFriendsListItem} /> */}
-            </ScrollView>
+            <View style={styles.addFriendsWrapper}>
+                <SectionList
+                    sections={sections}
+                    alwaysBounceVertical={false}
+                    renderItem={renderItem} />
+            </View>
         </View>
     )
 }
