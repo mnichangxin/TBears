a5d830787a7a98699f67b33150bc700238c55944
mnichangxin <mnichangxin@163.com>
2020-02-27 10:35:26 +0800

    Add SDWebImage support

--- ios/TBears/View/ChatMessage/TChatMessageCell.m
+++ ios/TBears/View/ChatMessage/TChatMessageCell.m
@@ -1,14 +1,15 @@
 #import "TChatMessageCell.h"
 #import "UIView+TL.h"
 #import "UIColor+Hex.h"
+#import <SDWebImage/SDWebImage.h>
 
-#define kAvatarImageWidth                          kScreenWidth * (132 / 1080)
+#define kAvatarImageWidth                          kScreenWidth * (132.f / 1080)
 #define kAvatarImageHeight                         kAvatarImageWidth
-#define kAvatarImageMarginLeft                     kScreenWidth * (37 / 1080)
-#define kAvatarImageMarginRight                    kScreenWidth * (53 / 1080)
-#define kMessageBackgroundViewMarginLeft           kScreenWidth * (36 / 1080)
+#define kAvatarImageMarginLeft                     kScreenWidth * (37.f / 1080)
+#define kAvatarImageMarginRight                    kScreenWidth * (53.f / 1080)
+#define kMessageBackgroundViewMarginLeft           kScreenWidth * (36.f / 1080)
 #define kMessageBackgroundViewMarginRight          kMessageBackgroundViewMarginLeft
-#define kMessageCellVerticalPadding                kScreenHeight * (103 / 1920)
+#define kMessageCellVerticalPadding                kScreenHeight * (103.f / 1920)
 
 @implementation TChatMessageCell
 
@@ -38,11 +39,11 @@
     
     switch (_messageModel.ownerType) {
         case TMessageOwnerTypeSelf:
-            [[self avatarImageView] setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:_messageModel.from.avatarUri]]];
+            [[self avatarImageView] sd_setImageWithURL:_messageModel.from.avatarUri];
             [[self messageBackgroundView] setBackgroundColor:[UIColor colorWithHexString:@"#564F5F"]];
             break;
         case TMessageOwnerTypeOther:
-            [[self avatarImageView] setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:_messageModel.from.avatarUri]]];
+            [[self avatarImageView] sd_setImageWithURL:_messageModel.from.avatarUri];
             [[self messageBackgroundView] setBackgroundColor:[UIColor colorWithHexString:@"#F3F3F3"]];
             break;
         case TMessageOwnerTypeSysyem:
